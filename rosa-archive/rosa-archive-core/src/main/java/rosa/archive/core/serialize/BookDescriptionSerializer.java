630444f70cc3f6fd9c0c509ce0d5e4a5e7cfe9e5
John Abrahams <jabrah23@gmail.com>
2014-10-09 14:50:52 -0400

    BookDescription writer

--- rosa-archive/rosa-archive-core/src/main/java/rosa/archive/core/serialize/BookDescriptionSerializer.java
+++ rosa-archive/rosa-archive-core/src/main/java/rosa/archive/core/serialize/BookDescriptionSerializer.java
@@ -1,6 +1,7 @@
 package rosa.archive.core.serialize;
 
 import com.google.inject.Inject;
+import com.sun.org.apache.xml.internal.serializer.OutputPropertiesFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,6 +14,15 @@ import rosa.archive.model.BookDescription;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -52,8 +62,78 @@ public class BookDescriptionSerializer implements Serializer<BookDescription> {
     }
 
     @Override
-    public void write(BookDescription object, OutputStream out) throws IOException {
-        throw new UnsupportedOperationException("Not implemented");
+    public void write(BookDescription description, OutputStream out) throws IOException {
+
+        Document doc = newDocument();
+
+        Element root = doc.createElement("TEI");
+        root.setAttribute("xmlns", "http://www.tei-c.org/ns/1.0");
+        root.setAttribute("version", "5.0");
+        doc.appendChild(root);
+
+        Element teiheader = doc.createElement("teiheader");
+        root.appendChild(teiheader);
+
+        Element fileDesc = doc.createElement("fileDesc");
+        teiheader.appendChild(fileDesc);
+
+        Element notesStmt = doc.createElement("notesStmt");
+        fileDesc.appendChild(notesStmt);
+
+        Map<String, Element> notes = description.getNotes();
+        for (String key : notes.keySet()) { 
+            notesStmt.appendChild(notes.get(key));
+        }
+
+        write(doc, out);
+
+    }
+
+    /**
+     * @return a new DOM document
+     */
+    private Document newDocument() {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            return null;
+        }
+
+        // TODO ask if it is fine to do it this way
+        // These elements stored in the map belong to a different Document object!
+        Document doc = builder.newDocument();
+        doc.setStrictErrorChecking(false);
+        return doc;
+    }
+
+    /**
+     * @param doc document
+     * @param out output stream
+     */
+    private void write(Document doc, OutputStream out) {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = transformerFactory.newTransformer();
+
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            // Options to make it human readable
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputPropertiesFactory.S_KEY_INDENT_AMOUNT, "4");
+        } catch (TransformerConfigurationException e) {
+            return;
+        }
+
+        Source xmlSource = new DOMSource(doc);
+        Result result = new StreamResult(out);
+
+        try {
+            transformer.transform(xmlSource, result);
+        } catch (TransformerException e) {
+            return;
+        }
     }
 
     private BookDescription buildDescription(Document document, List<String> errors) {
--- rosa-archive/rosa-archive-core/src/test/java/rosa/archive/core/serialize/BookDescriptionSerializerTest.java
+++ rosa-archive/rosa-archive-core/src/test/java/rosa/archive/core/serialize/BookDescriptionSerializerTest.java
@@ -1,14 +1,27 @@
 package rosa.archive.core.serialize;
 
+import com.sun.org.apache.xerces.internal.dom.ElementImpl;
 import org.junit.Before;
 import org.junit.Test;
-import rosa.archive.core.config.AppConfig;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import rosa.archive.model.BookDescription;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -17,10 +30,14 @@ public class BookDescriptionSerializerTest extends BaseSerializerTest {
 
     private BookDescriptionSerializer serializer;
 
+    String[] sections = {
+            "IDENTIFICATION", "BASIC INFORMATION", "QUIRES", "MATERIAL", "LAYOUT", "SCRIPT", "DECORATION",
+            "BINDING", "HISTORY", "TEXT"
+    };
+
     @Before
     public void setup() {
-        AppConfig config = mock(AppConfig.class);
-
+        super.setup();
         serializer = new BookDescriptionSerializer(config);
     }
 
@@ -32,8 +49,60 @@ public class BookDescriptionSerializerTest extends BaseSerializerTest {
             BookDescription description = serializer.read(in, errors);
 
             assertNotNull(description);
-            System.out.println(description.toString());
+            assertEquals(10, description.getNotes().size());
+
+            for (String section : sections) {
+                assertTrue(description.getNotes().containsKey(section));
+            }
         }
+    }
+
+    @Test
+    public void writeTest() throws Exception {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+//        Document doc = builder.newDocument();
+        Document doc = null;
+
+        URL url = getClass().getClassLoader().getResource("data/LudwigXV7/LudwigXV7.description_en.xml");
+        assertNotNull(url);
+
+        BookDescription description = null;
+        try (InputStream in = Files.newInputStream(Paths.get(url.toURI()))) {
+//            doc = builder.parse(in);
+
+            List<String> errors = new ArrayList<>();
+            description = serializer.read(in, errors);
+            System.out.println(description);
+        }
+
+        serializer.write(description, System.out);
+
+//        BookDescription description = new BookDescription();
+//        Map<String, Element> elementMap = description.getNotes();
+//
+//        description.setId("LudwigXV7.description_en.xml");
+//
+
+//        NodeList nodes = doc.getElementsByTagName("note");
+//        for (int i = 0; i < nodes.getLength(); i++) {
+//            Node note = nodes.item(i);
+//
+//            if (note.getNodeType() != Node.ELEMENT_NODE) {
+//                continue;
+//            }
+//
+//            Element el = (Element) note;
+//            String rend = el.getAttribute("rend");
+//
+//            if (rend == null || rend.equals("")) {
+//                continue;
+//            }
+//
+//
+//
+//        }
+
+
 
     }
 
