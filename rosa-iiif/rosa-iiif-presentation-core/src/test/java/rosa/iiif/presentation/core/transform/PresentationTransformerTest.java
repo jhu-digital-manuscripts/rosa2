e90b554ddea8132dbdfcbd0dfa59a51dd8dfb51d
John Abrahams <jabrah23@gmail.com>
2015-01-22 14:29:56 -0500

    Update tests for presentation transformer.

--- rosa-iiif/rosa-iiif-presentation-core/src/test/java/rosa/iiif/presentation/core/transform/PresentationTransformerTest.java
+++ rosa-iiif/rosa-iiif-presentation-core/src/test/java/rosa/iiif/presentation/core/transform/PresentationTransformerTest.java
@@ -2,43 +2,24 @@ package rosa.iiif.presentation.core.transform;
 
 import org.junit.Before;
 import org.junit.Test;
-import rosa.archive.model.Book;
-import rosa.archive.model.BookCollection;
-import rosa.archive.model.BookImage;
-import rosa.archive.model.BookText;
-import rosa.archive.model.CharacterNames;
-import rosa.archive.model.IllustrationTitles;
-import rosa.archive.model.ImageList;
-import rosa.archive.model.NarrativeSections;
-import rosa.archive.model.Permission;
-import rosa.archive.model.aor.AnnotatedPage;
-import rosa.archive.model.aor.Errata;
+import rosa.archive.core.BaseArchiveTest;
 import rosa.archive.model.aor.Location;
-import rosa.archive.model.aor.Marginalia;
-import rosa.archive.model.aor.MarginaliaLanguage;
-import rosa.archive.model.aor.Mark;
-import rosa.archive.model.aor.Numeral;
-import rosa.archive.model.aor.Position;
-import rosa.archive.model.aor.Symbol;
-import rosa.archive.model.aor.Underline;
-import rosa.archive.model.meta.BiblioData;
-import rosa.archive.model.meta.MultilangMetadata;
 import rosa.iiif.presentation.core.IIIFRequestFormatter;
 import rosa.iiif.presentation.core.JhuFsiImageIdMapper;
+import rosa.iiif.presentation.model.AnnotationList;
 import rosa.iiif.presentation.model.Canvas;
+import rosa.iiif.presentation.model.IIIFNames;
 import rosa.iiif.presentation.model.Manifest;
 import rosa.iiif.presentation.model.Reference;
 import rosa.iiif.presentation.model.Sequence;
-import rosa.iiif.presentation.model.ViewingDirection;
+import rosa.iiif.presentation.model.TextValue;
 import rosa.iiif.presentation.model.annotation.Annotation;
 import rosa.iiif.presentation.model.annotation.AnnotationTarget;
 import rosa.iiif.presentation.model.selector.FragmentSelector;
 import rosa.iiif.presentation.model.selector.Selector;
-import rosa.iiif.presentation.model.HtmlValue;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -47,29 +28,18 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-public class PresentationTransformerTest {
+public class PresentationTransformerTest extends BaseArchiveTest {
     private static final String ENDPOINT_SCHEME = "http";
     private static final String ENDPOINT_HOST = "example.org";
     private static final String ENDPOINT_PREFIX = "/iiif";
     private static final int ENDPOINT_PORT = -1;
-
-    private static final String FAKE_COLLECTION_ID_ON_IMAGE_SERVER = "col";
-    private static final String FAKE_COLLECTION_ID = "COLLECTION";
-    private static final String FAKE_BOOK_ID = "BOOK";
-
-    private static final String[] imageNames = {
-            "BOOK.001r.tif", "BOOK.001v.tif", "BOOK.002r.tif", "BOOK.002v.tif",
-            "BOOK.003r.tif", "BOOK.003v.tif", "BOOK.004r.tif", "BOOK.004v.tif",
-            "BOOK.005r.tif", "BOOK.005v.tif", "BOOK.006r.tif", "BOOK.006v.tif",
-            "BOOK.007r.tif", "BOOK.007v.tif", "BOOK.008r.tif", "BOOK.008v.tif"
-    };
 
     private PresentationTransformer transformer;
 
     @Before
     public void setup() {
         Map<String, String> idMap = new HashMap<>();
-        idMap.put(FAKE_COLLECTION_ID, FAKE_COLLECTION_ID_ON_IMAGE_SERVER);
+        idMap.put(VALID_COLLECTION, "valid");
 
         transformer = new PresentationTransformer(
                 new IIIFRequestFormatter(ENDPOINT_SCHEME, ENDPOINT_HOST, ENDPOINT_PREFIX, ENDPOINT_PORT),
@@ -80,106 +50,102 @@ public class PresentationTransformerTest {
     }
 
     @Test
-    public void buildSequenceTest() {
-        Sequence sequence = transformer.sequence(createBookCollection(), createBook(), "reading-order");
-        checkId(sequence.getId());
-
-        assertNotNull("List of canvases was missing.", sequence.getCanvases());
-        assertFalse("List of canvases was empty.", sequence.getCanvases().isEmpty());
-        for (Canvas c : sequence.getCanvases()) {
-            checkId(c.getId());
-            assertTrue(c.getWidth() > 0);
-            assertTrue(c.getHeight() > 0);
-            assertNotNull(c.getImages());
-        }
+    public void sequenceLudwigXV7Test() throws IOException {
+        checkSequence(transformer.sequence(loadValidCollection(), loadValidLudwigXV7(), "reading-order"));
     }
 
     @Test
-    public void transformerTest() {
-        Manifest manifest = transformer.manifest(createBookCollection(), createBook());
+    public void canvasLudwigXV7Test() throws IOException {
+        checkACanvas(transformer.canvas(loadValidCollection(), loadValidLudwigXV7(), "001v"));
+    }
+
+    @Test
+    public void manifestLudwigXV7Test() throws IOException {
+        Manifest manifest = transformer.manifest(loadValidCollection(), loadValidLudwigXV7());
         checkId(manifest.getId());
+        assertNotNull("Metadata for manifest is missing.", manifest.getMetadata());
+        assertFalse("Metadata for manifest is empty.", manifest.getMetadata().isEmpty());
+        checkTextValue(manifest.getLabel());
 
-        assertNotNull("Default sequence missing.", manifest.getDefaultSequence());
-        assertEquals("Wrong number of sequences.", 0, manifest.getOtherSequences().size());
-        assertNotNull("Default sequence missing.", manifest.getDefaultSequence());
+        assertNotNull("Default sequence is missing.", manifest.getDefaultSequence());
+        assertNotNull("", manifest.getOtherSequences());
+        assertTrue("Unexpected other sequences found.", manifest.getOtherSequences().isEmpty());
+        checkSequence(manifest.getDefaultSequence());
+    }
 
-        // Test sequence
-        Sequence seq = manifest.getDefaultSequence();
-        assertNotNull("No default sequence in Manifest.", seq);
+    @Test
+    public void annotationListLudwigXV7Test() throws IOException {
+        checkAnnotationList(transformer.annotationList(loadValidCollection(), loadValidLudwigXV7(), "002r", "all"));
+    }
+
+    @Test
+    public void annotationListFolgersHa2Test() throws IOException {
+        checkAnnotationList(transformer.annotationList(loadValidCollection(), loadValidFolgersHa2(), "001r", "all"));
+        checkAnnotationList(transformer.annotationList(loadValidCollection(), loadValidFolgersHa2(), "001r", "symbol"));
+        checkAnnotationList(transformer.annotationList(loadValidCollection(), loadValidFolgersHa2(), "001r", "marginalia"));
+        checkAnnotationList(transformer.annotationList(loadValidCollection(), loadValidFolgersHa2(), "001r", "underline"));
+    }
+
+    private void checkSequence(Sequence seq) {
         checkId(seq.getId());
-        assertTrue(seq.getStartCanvas() != -1);
-        assertEquals("Incorrect viewing direction.",
-                ViewingDirection.LEFT_TO_RIGHT, seq.getViewingDirection());
-        assertNotNull("Sequence ID not set.", seq.getId());
+        assertNotNull("List of canvases is missing from sequence.", seq.getCanvases());
+        assertFalse("List of canvases is empty.", seq.getCanvases().isEmpty());
 
-        // Test canvases
-        List<Canvas> canvases = seq.getCanvases();
-        assertNotNull("List of canvases missing from sequence.", canvases);
-        assertTrue("Wrong number of canvases.", canvases.size() == 16);
+        checkTextValue(seq.getLabel());
 
-        for (Canvas c : canvases) {
-            checkId(c.getId());
+        assertTrue("Start canvas is less than 0.", seq.getStartCanvas() >= 0);
+        checkACanvas(seq.getCanvases().get(seq.getStartCanvas()));
+    }
 
-            assertNotNull("List of image annotations missing.", c.getImages());
-            assertEquals("Too many image annotations.", 1, c.getImages().size());
+    private void checkACanvas(Canvas canvas) {
+        checkId(canvas.getId());
 
-            for (Annotation imageAnno : c.getImages()) {
-                checkId(imageAnno.getId());
+        checkTextValue(canvas.getLabel());
+        assertEquals("Unexpected object type found.", IIIFNames.SC_CANVAS, canvas.getType());
 
-                assertTrue(imageAnno.getDefaultSource().isImage());
-                String sourceId = imageAnno.getDefaultSource().getUri();
-                // TODO sucky
-                assertTrue(sourceId.contains(FAKE_COLLECTION_ID_ON_IMAGE_SERVER + "%2f" + FAKE_BOOK_ID + "%2f"));
-            }
+        assertTrue("Canvas must have a width > 0", canvas.getWidth() > 0);
+        assertTrue("Canvas must have a height > 0", canvas.getHeight() > 0);
 
-            Annotation imageAnno = c.getImages().get(0);
-            assertNotNull("Image annotation missing", imageAnno);
-            checkId(imageAnno.getId());
-            assertEquals("Incorrect motivation.", "sc:painting", imageAnno.getMotivation());
-            assertEquals("Incorrect image width.", 1000, imageAnno.getWidth());
-            assertEquals("Incorrect image height.", 1500, imageAnno.getHeight());
-            assertNotNull("Annotation source missing.", imageAnno.getDefaultSource());
-            assertFalse("Image must not be text!", imageAnno.getDefaultSource().isEmbeddedText());
-            assertTrue("Image is not an image.", imageAnno.getDefaultSource().isImage());
-            assertNull("Image cannot have selector.", imageAnno.getDefaultSource().getSelector());
-            // test for IIIF image service
-            assertEquals("Wrong number of sources.", 1, imageAnno.getSources().size());
-            assertNotNull("Annotation target missing.", imageAnno.getDefaultTarget());
-            assertEquals("Wrong number of targets.", 1, imageAnno.getTargets().size());
-            assertNull("Target has a Selector.", imageAnno.getDefaultTarget().getSelector());
-            assertFalse("Target is specific resource.", imageAnno.getDefaultTarget().isSpecificResource());
+        assertNotNull("List of images on canvas is missing.", canvas.getImages());
+        assertFalse("List of images is empty.", canvas.getImages().isEmpty());
 
-            assertEquals("Canvas width incorrect.", 2000, c.getWidth());
-            assertEquals("Canvas height incorrect.", 3000, c.getHeight());
-
-            assertNotNull("List of other content annotations is missing.",
-                    c.getOtherContent()
-            );
-            assertEquals("Wrong number of annotations from Annotated Pages.", 1, c.getOtherContent().size());
-            for (Reference ref : c.getOtherContent()) {
-                assertNotNull("Reference to AoR annotation missing.", ref);
-                checkId(ref.getReference());
-
-                assertEquals("sc:AnnotationList", ref.getType());
-                assertNotNull(ref.getLabel());
+        if (canvas.getOtherContent() != null) {
+            for (Reference ref : canvas.getOtherContent()) {
+                checkReference(ref);
             }
         }
+    }
 
-        assertNotNull("Metadata missing", manifest.getMetadata());
-        Map<String, HtmlValue> metadata = manifest.getMetadata();
-        assertEquals("Wrong number of pieces of metadata.", 16, metadata.size());
-        assertEquals("Wrong value for 'current location'",
-                "Current Location", metadata.get("currentLocation").getValue());
-        assertEquals("Wrong end year.", "1920", metadata.get("yearEnd").getValue());
+    private void checkAnnotationList(AnnotationList list) {
+        assertNotNull("Annotation list does not exist.", list);
 
-        String[] expectedFields = {
-                "title", "numberOfPages", "numberOfIllustrations", "width", "height",
-                "yearStart", "yearEnd", "dimensionUnits", "dimensions", "material",
-                "currentLocation", "shelfmark", "type"
-        };
-        for (String field : expectedFields) {
-            assertTrue("Expected field is missing. " + field, metadata.containsKey(field));
+        checkId(list.getId());
+        checkTextValue(list.getLabel());
+        assertEquals("Unexpected object type found.", IIIFNames.SC_ANNOTATION_LIST, list.getType());
+
+        for (Annotation ann : list) {
+            checkAnnotation(ann);
         }
+    }
+
+    private void checkAnnotation(Annotation annotation) {
+        checkId(annotation.getId());
+        assertEquals("Unexpected object type found.", IIIFNames.OA_ANNOTATION, annotation.getType());
+        assertNotNull("No default source found.", annotation.getDefaultSource());
+        assertNotNull("No default target found.", annotation.getDefaultTarget());
+    }
+
+    private void checkReference(Reference ref) {
+        checkTextValue(ref.getLabel());
+        assertNotNull("Reference type is missing.", ref.getType());
+        assertFalse("Reference type is empty.", ref.getType().isEmpty());
+        assertNotNull("Reference uri is missing.", ref.getReference());
+        assertFalse("Reference uri is empty.", ref.getReference().isEmpty());
+    }
+
+    private void checkTextValue(TextValue value) {
+        assertNotNull("Text value is missing.", value);
+        assertFalse("Text is empty.", value.getValue().isEmpty());
     }
 
     @Test
@@ -232,156 +198,4 @@ public class PresentationTransformerTest {
         assertTrue("ID has bad format.",
                 id.startsWith(ENDPOINT_SCHEME + "://" + ENDPOINT_HOST + ENDPOINT_PREFIX + "/"));
     }
-
-    private BookCollection createBookCollection() {
-        BookCollection collection = new BookCollection();
-        collection.setId(FAKE_COLLECTION_ID);
-
-        collection.setBooks(new String[] {FAKE_BOOK_ID});
-        collection.setLanguages(new String[] {"en", "fr"});
-        collection.setCharacterNames(new CharacterNames());
-        collection.setIllustrationTitles(new IllustrationTitles());
-        collection.setNarrativeSections(new NarrativeSections());
-
-        return collection;
-    }
-
-    private Book createBook() {
-        Book book = new Book();
-        book.setId(FAKE_BOOK_ID);
-
-        book.setImages(imageList());
-        book.setAnnotatedPages(annotatedPages());
-        book.setMultilangMetadata(metadata());
-        book.addPermission(permission(), "en");
-        book.addPermission(permission(), "fr");
-
-        return book;
-    }
-
-    private ImageList imageList() {
-        ImageList list = new ImageList();
-        list.setId("Book.images.csv");
-
-        List<BookImage> images = list.getImages();
-        for (String im : imageNames) {
-            BookImage image = new BookImage();
-
-            image.setId(im);
-            image.setMissing(false);
-            image.setWidth(1000);
-            image.setHeight(1500);
-
-            images.add(image);
-        }
-
-        list.setImages(images);
-        return list;
-    }
-
-    private List<AnnotatedPage> annotatedPages() {
-        List<AnnotatedPage> pages = new ArrayList<>();
-
-        for (String image : imageNames) {
-            AnnotatedPage page = new AnnotatedPage();
-
-            page.setId(image.replace(".tif", ".xml"));
-            page.setPage(image);
-
-            for (int i = 0; i < 20; i++) {
-                page.getUnderlines().add(new Underline("Text line", "pen", "TYPE", "en"));
-                page.getUnderlines().add(new Underline("Text line", "pen", "TYPE", "fr"));
-
-                page.getSymbols().add(new Symbol("Text line", "NAME", Location.INTEXT));
-                page.getSymbols().add(new Symbol("Text line", "NAME", Location.LEFT_MARGIN));
-
-                page.getMarks().add(new Mark("Text line", "NAME", "METHOD", "en", Location.INTEXT));
-                page.getMarks().add(new Mark("Text line", "NAME", "METHOD", "fr", Location.RIGHT_MARGIN));
-
-                page.getNumerals().add(new Numeral("Text line", Location.HEAD));
-
-                page.getErrata().add(new Errata("Copy text", "amended text"));
-
-                Marginalia marg = new Marginalia();
-                marg.setTranslation("Translation");
-                marg.setReferringText("Text line");
-                marg.setHand("Harvey Dent");
-//                marg.setLocation(Location.TAIL);
-
-                MarginaliaLanguage lang = new MarginaliaLanguage();
-                Position pos = new Position();
-                pos.getTexts().add("This is marginalia text.");
-                pos.setPlace(Location.TAIL);
-                lang.getPositions().add(pos);
-                marg.getLanguages().add(lang);
-
-                page.getMarginalia().add(marg);
-            }
-            pages.add(page);
-        }
-
-        return pages;
-    }
-
-    private MultilangMetadata metadata() {
-        MultilangMetadata metadata = new MultilangMetadata();
-        metadata.setId("BOOK.description.xml");
-
-        metadata.setNumberOfPages(100);
-        metadata.setNumberOfIllustrations(10);
-        metadata.setWidth(1000);
-        metadata.setHeight(1500);
-        metadata.setYearStart(1900);
-        metadata.setYearEnd(1920);
-        metadata.setDimensionUnits("mm");
-
-        BiblioData en = new BiblioData();
-        en.setCurrentLocation("Current Location");
-        en.setTitle("BOOK TITLE");
-        en.setLanguage("en");
-        en.setMaterial("Material");
-        en.setDateLabel("Date");
-        en.setOrigin("Origin");
-        en.setRepository("Repository");
-        en.setShelfmark("Shelfmark");
-        en.setType("Book");
-
-        BiblioData fr = new BiblioData();
-        fr.setCurrentLocation("Current Location");
-        fr.setTitle("BOOK TITLE");
-        fr.setLanguage("en");
-        fr.setMaterial("Material");
-        fr.setDateLabel("Date");
-        fr.setOrigin("Origin");
-        fr.setRepository("Repository");
-        fr.setShelfmark("Shelfmark");
-        fr.setType("Book");
-
-        Map<String, BiblioData> dataMap = new HashMap<>();
-        dataMap.put("en", en);
-        dataMap.put("fr", fr);
-
-        metadata.setBiblioDataMap(dataMap);
-
-        BookText text1 = new BookText();
-        text1.setColumnsPerPage(2);
-        text1.setFirstPage("1");
-        text1.setLastPage("100");
-        text1.setNumberOfPages(100);
-        text1.setNumberOfIllustrations(10);
-        text1.setTitle("BOOK TITLE");
-        text1.setTextId("text_id");
-
-        metadata.getBookTexts().add(text1);
-
-        return metadata;
-    }
-
-    private Permission permission() {
-        Permission p = new Permission();
-        p.setId("BOOK.permission.html");
-        p.setPermission("This is a permission statement.");
-        return p;
-    }
-
 }
