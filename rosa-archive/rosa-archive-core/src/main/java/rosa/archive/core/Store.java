42de53ad7c32b0bac30a5c1e54bf1a06ec8b946b
John Abrahams <jabrah23@gmail.com>
2015-02-11 13:09:43 -0500

    Javadoc on Store interface

--- rosa-archive/rosa-archive-core/src/main/java/rosa/archive/core/Store.java
+++ rosa-archive/rosa-archive-core/src/main/java/rosa/archive/core/Store.java
@@ -35,6 +35,7 @@ public interface Store {
      * Get a single book from the archive that exists in the specified collection.
      *
      * @param collection
+     *          id of the collection
      * @param bookId
      *          id of the book to get
      * @return
@@ -84,18 +85,129 @@ public interface Store {
 
     boolean updateChecksum(BookCollection collection, Book book, boolean force, List<String> errors) throws IOException;
 
+    /**
+     * Generate an image list of images in the book. This image list records
+     * the file name, dimensions, and ordering of the images.
+     *
+     * @param collection id of the collection
+     * @param book id of the book
+     * @param force force the operation?
+     * @param errors list of errors
+     * @throws IOException
+     */
     void generateAndWriteImageList(String collection, String book, boolean force, List<String> errors) throws IOException;
 
+    /**
+     * Generate an image list for cropped images.
+     *
+     * @param collection id of the collection
+     * @param book id of the book
+     * @param force force the operation?
+     * @param errors list of errors
+     * @throws IOException
+     */
     void generateAndWriteCropList(String collection, String book, boolean force, List<String> errors) throws IOException;
 
+    /**
+     * Crop images in a book. If cropping information is available for the images
+     * of a book, this operation will copy the images into the book's 'cropped'
+     * location and crop those images. ImageMagick is required for this
+     * operation.
+     *
+     * @param collection id of the collection
+     * @param book id of the book
+     * @param force force the operation to complete?
+     * @param errors list of errors found while cropping
+     * @throws IOException
+     */
     void cropImages(String collection, String book, boolean force, List<String> errors) throws IOException;
 
+    /**
+     * <p>
+     * Generate a file map to map file names of images. The file map associates original image names
+     * with new image names that are formatted to work with the archive tools and API. The
+     * naming scheme that this tool follows can be seen at <br/>
+     * <a href="https://github.com/jhu-digital-manuscripts/rosa2/wiki/Rose-File-Naming">
+     *      https://github.com/jhu-digital-manuscripts/rosa2/wiki/Rose-File-Naming
+     * </a>
+     * </p>
+     *
+     * <p>
+     * This function makes certain assumptions about the original naming scheme of the images.
+     * <ul>
+     * <li>All images are in normal/reading order.</li>
+     * <li>Front cover will be the first image, if it exists.</li>
+     * <li>If there is a front cover, it will always be followed by a front matter pastedown.</li>
+     * <li>Misc images are last to appear in the image list, if at all.</li>
+     * <li>If a back cover exists, it will always be proceeded by an end matter pastedown.</li>
+     * <li>Back cover will be the image immediately before any misc images, or the last
+     * image if no misc images exist.</li>
+     * </ul>
+     * </p>
+     *
+     * <p>
+     * If these conditions are true, then this tool should work just fine. However, it may
+     * be common that these conditions are not necessarily true. If this is the case, the
+     * images can be manually changed in order to fit these conditions.
+     * </p>
+     *
+     * <p>
+     * For example, if image one in the original naming scheme is a misc image of a color bar and
+     * image two in the original naming scheme is an image of the spine, and the rest of
+     * the images fit the above conditions, the first two images can be manually moved to
+     * a temp location, outside of the book location. The tool can then be run on the remaining
+     * images in the book. After the file map is generated, it can be manually edited to
+     * include the two images that were moved. These last two images must be moved back
+     * into the book location and renamed by hand.
+     * </p>
+     *
+     * @param collection name of collection
+     * @param book name of book
+     * @param newId new ID to use when renaming files
+     * @param hasFrontCover does the book have images of the front cover + pastedown?
+     * @param hasBackCover does the book have images of the back cover + pastedown?
+     * @param numFrontmatter number of front matter flyleaves
+     * @param numEndmatter number of end matter flyleaves
+     * @param numMisc number of misc items
+     * @param errors list of errors found while generating file map
+     * @throws IOException
+     */
     void generateFileMap(String collection, String book, String newId, boolean hasFrontCover, boolean hasBackCover,
                          int numFrontmatter, int numEndmatter, int numMisc, List<String> errors) throws IOException;
 
+    /**
+     * Validate AoR transcriptions against the schema.
+     *
+     * @param collection id of the collection
+     * @param book id of the book
+     * @param errors list of errors found during validation
+     * @param warnings list of warnings found during validation
+     * @throws IOException
+     */
     void validateXml(String collection, String book, List<String> errors, List<String> warnings) throws IOException;
 
+    /**
+     * Rename images of a book according to a file map. Does nothing if the file map
+     * does not exist.
+     *
+     * @param collection id of the collection
+     * @param book id of the book
+     * @param dryRun .
+     * @param changeId .
+     * @param errors list of errors
+     * @throws IOException
+     */
     void renameImages(String collection, String book, boolean dryRun, boolean changeId, List<String> errors) throws IOException;
 
+    /**
+     * Rename the AoR transcription files according to the file map. The files are renamed
+     * and the file references in the transcriptions are also changed according to
+     * the information in the file map. Does nothing if no file map is found.
+     *
+     * @param collection id of the collection
+     * @param book id of the book
+     * @param errors list of errors found while renaming
+     * @throws IOException
+     */
     void renameTranscriptions(String collection, String book, List<String> errors) throws IOException;
 }
