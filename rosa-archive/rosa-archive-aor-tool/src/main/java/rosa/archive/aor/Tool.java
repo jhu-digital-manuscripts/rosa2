d0fb53191e6bae2b8faeb69d1d2f4ec65877d9de
Mark Patton <mark.patton@gmail.com>
2015-02-23 13:25:19 -0500

    Add more columns to AoR stats and cleanups.

--- rosa-archive/rosa-archive-aor-tool/src/main/java/rosa/archive/aor/Tool.java
+++ rosa-archive/rosa-archive-aor-tool/src/main/java/rosa/archive/aor/Tool.java
@@ -4,36 +4,36 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Tool {
-	public static void main(String[] args) throws Exception {
-		if (args.length < 1) {
-			System.err.println("Usage: stats|validate arg...");
-			System.exit(1);
-		}
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.err.println("Usage: stats|validate arg...");
+            System.exit(1);
+        }
 
-		String command = args[0];
+        String command = args[0];
 
-		if (command.equals("stats")) {
-			if (args.length < 2) {
-				System.err.println("Usage: stats aor_book_dir...");
-				System.exit(1);
-			}
+        if (command.equals("stats")) {
+            if (args.length < 2) {
+                System.err.println("Usage: stats aor_book_dir...");
+                System.exit(1);
+            }
 
-			AorStatsCollector stats = new AorStatsCollector();
+            AorStatsCollector stats = new AorStatsCollector();
 
-			for (int i = 1; i < args.length; i++) {
-				Path book_path = Paths.get(args[i]);
-				String book_id = book_path.getFileName().toString();
+            for (int i = 1; i < args.length; i++) {
+                Path book_path = Paths.get(args[i]);
+                String book_id = book_path.getFileName().toString();
 
-				stats.collectBookStats(book_id, book_path);
-			}
+                stats.collectBookStats(book_id, book_path);
+            }
 
-			stats.writeBookStats(Paths.get("."));
-		} else if (command.equals("validate")) {
-			System.err.println("Not yet implemented");
-			System.exit(1);
-		} else {
-			System.err.println("Invalid command\nUsage: stats|validate arg...");
-			System.exit(1);
-		}
-	}
+            stats.writeBookStats(Paths.get("."));
+        } else if (command.equals("validate")) {
+            System.err.println("Not yet implemented");
+            System.exit(1);
+        } else {
+            System.err.println("Invalid command\nUsage: stats|validate arg...");
+            System.exit(1);
+        }
+    }
 }
