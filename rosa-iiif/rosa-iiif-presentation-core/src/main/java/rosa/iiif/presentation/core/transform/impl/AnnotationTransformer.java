2473f89c926b2e75da7407e4ab0b603925f8012b
John Abrahams <jabrah23@gmail.com>
2015-10-09 14:13:29 -0400

    AnnotationTransformer able to find archive annotation. Now must translate to IIIF annotation.

--- rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/transform/impl/AnnotationTransformer.java
+++ rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/transform/impl/AnnotationTransformer.java
@@ -2,15 +2,21 @@ package rosa.iiif.presentation.core.transform.impl;
 
 import com.google.inject.Inject;
 import rosa.archive.core.ArchiveNameParser;
+import rosa.archive.core.serialize.AORAnnotatedPageConstants;
 import rosa.archive.model.Book;
 import rosa.archive.model.BookCollection;
+import rosa.archive.model.BookImage;
+import rosa.archive.model.ImageList;
+import rosa.archive.model.aor.AnnotatedPage;
 import rosa.iiif.presentation.core.IIIFRequestFormatter;
 import rosa.iiif.presentation.core.transform.Transformer;
 import rosa.iiif.presentation.model.annotation.Annotation;
 
 import java.util.logging.Logger;
 
-public class AnnotationTransformer extends BasePresentationTransformer implements Transformer<Annotation> {
+public class AnnotationTransformer extends BasePresentationTransformer implements Transformer<Annotation>,
+        AORAnnotatedPageConstants {
+    private static final String ANNOTATION_ILLUSTRATION = "illustration";
     private static final Logger logger = Logger.getLogger(AnnotationTransformer.class.toString());
 
     private ArchiveNameParser imageNameParser;
@@ -25,7 +31,6 @@ public class AnnotationTransformer extends BasePresentationTransformer implement
     public Annotation transform(BookCollection collection, Book book, String name) {
         // Find annotation in book
 
-
         // Transform archive anno -> iiif anno
         return null;
     }
@@ -35,14 +40,96 @@ public class AnnotationTransformer extends BasePresentationTransformer implement
         return Annotation.class;
     }
 
+    /**
+     * Get the archive annotation that was named.
+     *
+     * @param book archive book
+     * @param annoName annotation name
+     * @return the archive annotation named
+     */
+    private rosa.archive.model.aor.Annotation getArchiveAnnotation(Book book, String annoName) {
+        AnnotatedPage annotatedPage = book.getAnnotationPage(getPage(annoName));
+
+        int id = getPrimaryNumber(annoName);
+        if (id < 0) {
+            return null;
+        }
+
+        switch (getAnnotationType(annoName)) {
+            case TAG_MARGINALIA:
+                int[] ids = getMarginaliaNums(annoName);
+                if (ids != null && ids.length == 3) {
+                    annotatedPage.getMarginalia().get(ids[0])
+                            .getLanguages().get(ids[1])
+                            .getPositions().get(ids[2])
+                            .getTexts();        // Transcription text here
+                }
+                break;
+            case TAG_UNDERLINE:
+                annotatedPage.getUnderlines().get(id);
+                break;
+            case TAG_SYMBOL:
+                annotatedPage.getSymbols().get(id);
+                break;
+            case TAG_MARK:
+                annotatedPage.getMarks().get(id);
+                break;
+            case TAG_NUMERAL:
+                annotatedPage.getNumerals().get(id);
+                break;
+            case TAG_ERRATA:
+                annotatedPage.getNumerals().get(id);
+                break;
+            case TAG_DRAWING:
+                annotatedPage.getDrawings().get(id);
+                break;
+            case ANNOTATION_ILLUSTRATION:
+                
+                break;
+            default:
+                break;
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the page on which the annotation lives from the annotation name.
+     *
+     * @param name annotation name
+     * @return the page associated with the annotation
+     */
     private String getPage(String name) {
         return name.split("_")[0];
     }
 
+    /**
+     * Get the annotation type from its name.
+     *
+     * <ul>
+     * <li>marginalia</li>
+     * <li>underline</li>
+     * <li>mark</li>
+     * <li>symbol</li>
+     * <li>errata</li>
+     * <li>numeral</li>
+     * <li>drawing</li>
+     * </ul>
+     *
+     * @param name annotation name
+     * @return the type of annotation
+     */
     private String getAnnotationType(String name) {
         return name.split("_")[1];
     }
 
+    /**
+     * Get the primary annotation ID number. Each annotation type will
+     * have at least one identifying number associated with it.
+     *
+     * @param name annotation name
+     * @return primary ID #
+     */
     private int getPrimaryNumber(String name) {
         String num = name.split("_")[2];
         try {
@@ -62,7 +149,7 @@ public class AnnotationTransformer extends BasePresentationTransformer implement
      * line of transcription, one must know:
      *
      * <ol>
-     * <li>Which marginalia</li>
+     * <li>Which marginalia (also the marginalia primary ID #)</li>
      * <li>Which language within the marginalia</li>
      * <li>Which position within the language</li>
      * </ol>
@@ -88,6 +175,23 @@ public class AnnotationTransformer extends BasePresentationTransformer implement
             logger.warning("Failed to parse a marginalia ID number. (" + name + ")");
         } catch (IndexOutOfBoundsException e) {
             logger.warning("Not enough parts in name to represent a marginalia annotation. (" + name + ")");
+        }
+
+        return null;
+    }
+
+    /**
+     * For a given page name, find the associated BookImage
+     *
+     * @param images list of book images
+     * @param page page name
+     * @return the BookImage
+     */
+    private BookImage getPageImage(ImageList images, String page) {
+        for (BookImage image : images) {
+            if (image.getName().equals(page)) {
+                return image;
+            }
         }
 
         return null;
