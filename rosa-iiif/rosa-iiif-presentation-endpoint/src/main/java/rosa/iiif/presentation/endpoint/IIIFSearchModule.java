2bed63a8a7604e2b913757eac56dfd7403080d82
John Abrahams <jabrah23@gmail.com>
2016-01-14 15:36:49 -0500

    Combined rosa-iiif-search module into rosa-iiif-presentation module. NOTE further merging is requried for the search service to be made available (web.xml and guice servlet configs need to be changed, probably servlet code as well)

---
+++ rosa-iiif/rosa-iiif-presentation-endpoint/src/main/java/rosa/iiif/presentation/endpoint/IIIFSearchModule.java
@@ -0,0 +1,96 @@
+package rosa.iiif.presentation.endpoint;
+
+import com.google.inject.Provides;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+import com.google.inject.servlet.ServletModule;
+import rosa.archive.core.ArchiveNameParser;
+import rosa.archive.core.Store;
+import rosa.iiif.presentation.core.IIIFRequestFormatter;
+import rosa.iiif.presentation.core.IIIFRequestParser;
+import rosa.iiif.presentation.core.transform.impl.AnnotationTransformer;
+import rosa.iiif.presentation.core.search.IIIFLuceneSearchAdapter;
+import rosa.iiif.presentation.core.search.IIIFSearchRequestFormatter;
+import rosa.iiif.presentation.core.search.IIIFSearchService;
+import rosa.iiif.presentation.core.search.RosaIIIFSearchService;
+import rosa.search.core.LuceneSearchService;
+import rosa.search.core.SearchService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class IIIFSearchModule extends ServletModule {
+    private static final Logger logger = Logger.getLogger("");
+    private static final String SERVLET_CONFIG_PATH = "/iiif-servlet.properties";
+
+    @Override
+    protected void configureServlets() {
+        logger.info("Configuring IIIF Search servlet.");
+        bind(ArchiveNameParser.class);
+        bind(IIIFRequestParser.class);
+        bind(AnnotationTransformer.class);
+
+        Names.bindProperties(binder(), loadProperties(SERVLET_CONFIG_PATH));
+
+        // TODO not serving anything, must set to not conflict with iiif-presentation
+//        serve("/*").with(IIIFSearchServlet.class);
+        logger.info("Done configuring servlet.");
+    }
+
+    @Provides
+    protected SearchService provideLuceneSearchService(@Named("search.index.path") String indexPath)
+            throws IOException {
+        return new LuceneSearchService(Paths.get(indexPath));
+    }
+
+    @Provides
+    protected IIIFLuceneSearchAdapter getLuceneAdapter(AnnotationTransformer annotationTransformer,
+                                                       Store archiveStore,
+                                                       @Named("formatter.presentation") IIIFRequestFormatter reqFormatter) {
+        return new IIIFLuceneSearchAdapter(annotationTransformer, archiveStore, reqFormatter);
+    }
+
+    @Provides
+    protected IIIFSearchService provideIIIFSearchService(@Named("collection.id") String collectionId,
+                                                         SearchService searchService,
+                                                         IIIFLuceneSearchAdapter adapter,
+                                                         Store store,
+                                                         IIIFSearchRequestFormatter requestFormatter) {
+        logger.info("Creating IIIF search service. (collection: " + collectionId + ")");
+        return new RosaIIIFSearchService(store, searchService, adapter, collectionId, requestFormatter);
+    }
+
+    @Provides
+    protected IIIFSearchRequestFormatter provideSearchRequestFormatter(@Named("iiif.search.scheme") String scheme,
+                                                                       @Named("iiif.search.host") String host,
+                                                                       @Named("iiif.search.prefix") String prefix,
+                                                                       @Named("iiif.search.port") int port) {
+        return new IIIFSearchRequestFormatter(scheme, host, prefix, port);
+    }
+
+    private Properties loadProperties(String path) {
+        logger.info("Loading servlet properties [" + path + "]");
+        Properties props = new Properties();
+
+        try (InputStream in = getClass().getResourceAsStream(path)) {
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load properties: " + path, e);
+        }
+
+        return props;
+    }
+
+    @Provides @Named("ignored.parameters")
+    public String[] getIgnoredParams(@Named("search.ignored") String ignored) {
+        if (ignored == null) {
+            return new String[0];
+        }
+
+        return ignored.split(",");
+    }
+
+}
---
+++ rosa-iiif/rosa-iiif-presentation-model/src/main/java/rosa/iiif/presentation/model/search/IIIFSearchResult.java
@@ -0,0 +1,167 @@
+package rosa.iiif.presentation.model.search;
+
+import rosa.iiif.presentation.model.AnnotationList;
+import rosa.iiif.presentation.model.IIIFNames;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ * IIIF Search API v0.9
+ *
+ * http://search.iiif.io/api/search/0.9/#presentation-api-compatible-responses
+ * http://search.iiif.io/api/search/0.9/#search-api-specific-responses
+ */
+public class IIIFSearchResult extends AnnotationList implements Serializable, IIIFSearchNames, IIIFNames {
+    private static final long serialVersionUID = 1L;
+
+    /** Total number of results found. Useful with paging. */
+    private long total;
+
+    /**
+     * Ignored parameters.
+     *
+     * IIIF Search requires parameter 'q'. 'motivation' is recommended and
+     * three parameters 'date', 'user', 'box' are optional. Any ignored parameter
+     * must be reported here.
+     */
+    private String[] ignored;
+
+    /** List of 'hits' information containing context for results. */
+    private IIIFSearchHit[] hits;
+
+    // Info for paging
+    /** Index of the first result in this page within the total */
+    private long startIndex;
+
+    // URIs for navigating results paging
+    /** URI of next page of results */
+    private String next;
+    /** URI of previous page of results */
+    private String prev;
+    /** URI of first results page */
+    private String first;
+    /** URI of last results page */
+    private String last;
+
+    public IIIFSearchResult() {
+        super();
+        setContext(SEARCH_CONTEXT);
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public String[] getIgnored() {
+        return ignored;
+    }
+
+    public void setIgnored(String[] ignored) {
+        this.ignored = ignored;
+    }
+
+    public IIIFSearchHit[] getHits() {
+        return hits;
+    }
+
+    public void setHits(IIIFSearchHit[] hits) {
+        this.hits = hits;
+    }
+
+    public long getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(long startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
+
+    public void setPrev(String prev) {
+        this.prev = prev;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    @Override
+    public boolean canEqual(Object o) {
+        return o instanceof IIIFSearchResult;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        IIIFSearchResult that = (IIIFSearchResult) o;
+
+        if (total != that.total) return false;
+        if (startIndex != that.startIndex) return false;
+        if (!Arrays.deepEquals(ignored, that.ignored)) return false;
+        if (!Arrays.deepEquals(hits, that.hits)) return false;
+        if (next != null ? !next.equals(that.next) : that.next != null) return false;
+        if (prev != null ? !prev.equals(that.prev) : that.prev != null) return false;
+        if (first != null ? !first.equals(that.first) : that.first != null) return false;
+        return !(last != null ? !last.equals(that.last) : that.last != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (int) (total ^ (total >>> 32));
+        result = 31 * result + (ignored != null ? Arrays.deepHashCode(ignored) : 0);
+        result = 31 * result + (hits != null ? Arrays.deepHashCode(hits) : 0);
+        result = 31 * result + (int) (startIndex ^ (startIndex >>> 32));
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        result = 31 * result + (prev != null ? prev.hashCode() : 0);
+        result = 31 * result + (first != null ? first.hashCode() : 0);
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "IIIFSearchResult{" +
+                "total=" + total +
+                ", ignored=" + Arrays.toString(ignored) +
+                ", hits=" + Arrays.toString(hits) +
+                ", startIndex=" + startIndex +
+                ", next='" + next + '\'' +
+                ", prev='" + prev + '\'' +
+                ", first='" + first + '\'' +
+                ", last='" + last + '\'' +
+                '}';
+    }
+}
