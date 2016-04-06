9cf5758646cbad113a98f7b19e9f0521e4a2f7ab
Mark Patton <mark.patton@gmail.com>
2016-04-06 14:45:04 -0400

    Add initial jhsearch support for rose and pizan.

--- rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/jhsearch/JHSearchLuceneMapper.java
+++ rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/jhsearch/JHSearchLuceneMapper.java
@@ -1,15 +1,29 @@
 package rosa.iiif.presentation.core.jhsearch;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.lucene.document.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
+import rosa.archive.core.util.TranscriptionSplitter;
 import rosa.archive.model.Book;
 import rosa.archive.model.BookCollection;
+import rosa.archive.model.BookDescription;
 import rosa.archive.model.BookImage;
+import rosa.archive.model.CharacterNames;
+import rosa.archive.model.Illustration;
+import rosa.archive.model.IllustrationTagging;
+import rosa.archive.model.IllustrationTitles;
 import rosa.archive.model.ImageList;
 import rosa.archive.model.ReferenceSheet;
 import rosa.archive.model.aor.AnnotatedPage;
@@ -32,6 +46,9 @@ import rosa.search.core.BaseLuceneMapper;
 import rosa.search.model.SearchField;
 import rosa.search.model.SearchFieldType;
 
+
+// TODO Duplication with WebsiteLuceneMapper
+
 /**
  * Index and create queries for data which becomes IIIF Presentation
  * Annotations.
@@ -40,7 +57,7 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
     private static final Logger logger = Logger.getLogger(JHSearchLuceneMapper.class.toString());
 
     private final IIIFPresentationRequestFormatter formatter;
-    
+
     public JHSearchLuceneMapper(IIIFPresentationRequestFormatter formatter) {
         super(JHSearchField.values());
         this.formatter = formatter;
@@ -65,23 +82,205 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
     public List<Document> createDocuments(BookCollection col, Book book) throws IOException {
         List<Document> result = new ArrayList<>();
 
-        // Create annotation documents annotations associated with each image
-
+        // Index information associated with canvases
+        
         ImageList images = book.getImages();
+        
+        // Create document for each image
+        Map<String, String> transcriptionMap = TranscriptionSplitter.split(book.getTranscription());
 
         if (images.getImages() == null) {
             logger.warning("No image list found. [" + col.getId() + ":" + book.getId() + "]");
         } else {
             for (BookImage image : images.getImages()) {
-                // AoR transcription
-                index(col, book, image, book.getAnnotationPage(image.getId()), result);
-
-                // TODO index other data
+                Document doc = create_canvas_document(col, book, image);
+                
+                String trans = transcriptionMap.get(getStandardPage(image));
+                
+                index(col, book, image, doc, trans);
+                result.add(doc);
             }
         }
 
+        // Index information associated with manifests
+        
+        Document doc = create_manifest_document(col, book);
+        index(col, book, doc);
+        result.add(doc);
+        
         return result;
     }
+
+    // TODO need better way of getting standard name... refer to how it is done in the transcription splitter
+    private String getStandardPage(BookImage image) {
+        String start = image.getName();
+        if (start.length() == 2) {
+            return "00" + start;
+        } else if (start.length() == 3) {
+            return "0" + start;
+        } else {
+            return start;
+        }
+    }
+
+    
+    private void index(BookDescription desc, Document doc) throws IOException {
+        try {
+            addField(doc, JHSearchField.DESCRIPTION, SearchFieldType.ENGLISH, xml_to_text(desc.getXML()));
+        } catch (SAXException e) {
+            throw new IOException(e);
+        }
+    }
+
+    /**
+     * Parse an XML document and return all textual content in a String.
+     *
+     * @param src source XML
+     * @return String of all textual content
+     * @throws SAXException
+     * @throws IOException
+     */
+    private static String xml_to_text(InputSource src) throws SAXException, IOException {
+        XMLReader r = XMLReaderFactory.createXMLReader();
+        final StringBuilder result = new StringBuilder();
+
+        r.setContentHandler(new DefaultHandler() {
+            public void characters(char[] text, int offset, int len)
+                    throws SAXException {
+                result.append(text, offset, len);
+            }
+        });
+
+        r.parse(src);
+
+        return result.toString();
+    }
+
+    private String xml_to_text(String xml) throws SAXException, IOException {
+        return xml_to_text(new InputSource(new StringReader(xml)));
+    }
+    
+    private void index(BookCollection col, Book book, Document doc) throws IOException {
+        BookDescription desc = book.getBookDescription("en");
+        
+        if (desc != null) {
+            index(desc, doc);
+        }
+    }
+    
+    private void index(BookCollection col, Book book, BookImage image, Document doc, String trans) {
+        AnnotatedPage page = book.getAnnotationPage(image.getId());
+        
+        if (page != null) {
+            index (col, book, image, doc, page);
+        }
+        
+        IllustrationTagging tag = book.getIllustrationTagging();
+        
+        if (tag  != null) {
+            index (col, book, image, doc, tag);
+        }
+        
+        
+        // Index transcription text that appears on this page
+        if (trans != null) {
+            try {
+                indexTranscriptionFragment(trans, doc);
+            } catch (SAXException | IOException e) {
+                logger.log(Level.SEVERE, "Failed to parse transcription fragment. ["
+                        + image.getName() + "]", e);
+            }
+        }
+
+    }
+
+    private void index(BookCollection col, Book book, BookImage image, Document doc, IllustrationTagging imgtag) {
+        IllustrationTitles titles = col.getIllustrationTitles();
+        CharacterNames char_names = col.getCharacterNames();
+
+        StringBuilder text = new StringBuilder();
+
+        for (int index: imgtag.findImageIndices(book, image.getId())) {
+            Illustration illus = imgtag.getIllustrationData(index);
+
+            for (String char_id: illus.getCharacters()) {
+                rosa.archive.model.CharacterName char_name = char_names
+                        .getCharacterName(char_id);
+
+                if (char_name == null) {
+                    text.append(char_id);
+                    text.append(", ");
+                } else {
+                    for (String name: char_name.getAllNames()) {
+                        text.append(name);
+                        text.append(", ");
+                    }
+                }
+            }
+
+            for (String title_id: illus.getTitles()) {
+                String title = titles.getTitleById(title_id);
+
+                if (title != null && !title.isEmpty()) {
+                    text.append(title);
+                    text.append(", ");
+                } else {
+                    text.append(title_id);
+                    text.append(", ");
+                }
+            }
+
+            text.append(illus.getTextualElement());
+            text.append(", ");
+            text.append(illus.getArchitecture());
+            text.append(", ");
+            text.append(illus.getCostume());
+            text.append(", ");
+            text.append(illus.getOther());
+            text.append(", ");
+            text.append(illus.getObject());
+            text.append(", ");
+            text.append(illus.getLandscape());
+            text.append(", ");
+        }
+
+        
+        if (text.length() > 0) {
+            addField(doc, JHSearchField.ILLUSTRATION, SearchFieldType.ENGLISH, text.toString());
+        }
+    }
+
+
+        
+    private void indexTranscriptionFragment(String transcription, Document doc) throws SAXException, IOException {
+        XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+
+        TranscriptionXMLReader trxml = new TranscriptionXMLReader();        
+        xmlReader.setContentHandler(trxml);
+        xmlReader.parse(new InputSource(new StringReader(transcription)));
+
+        addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.OLD_FRENCH, trxml.getPoetry());
+
+        if (trxml.hasCatchphrase()) {
+            addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.OLD_FRENCH, trxml.getCatchphrase());
+        }
+        
+        if (trxml.hasRubric()) {
+            addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.OLD_FRENCH, trxml.getRubric());
+        }
+        
+        if (trxml.hasIllus()) {
+            addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.ENGLISH, trxml.getIllustration());
+        }
+
+        addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.ENGLISH, trxml.getLecoy());
+        addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.ENGLISH, trxml.getLine());
+
+        if (trxml.hasNote()) {
+            addField(doc, JHSearchField.TRANSCRIPTION, SearchFieldType.ENGLISH, trxml.getNote());
+        }
+    }
+
 
     /**
      * Index all information about the AoR transcriptions.
@@ -118,14 +317,7 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
      * @param result
      *            resulting list of indexed documents
      */
-    private void index(BookCollection col, Book book, BookImage image, AnnotatedPage page,
-            List<Document> result) {
-        if (page == null) {
-            return;
-        }
-        
-        Document doc = create_document(page, col, book, image);
-
+    private void index(BookCollection col, Book book, BookImage image, Document doc, AnnotatedPage page) {
         page.getSymbols().forEach(a -> index(col, book, image, a, doc));
         page.getMarks().forEach(a -> index(col, book, image, a, doc));
         page.getDrawings().forEach(a -> index(col, book, image, a, doc));
@@ -133,27 +325,45 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
         page.getNumerals().forEach(a -> index(col, book, image, a, doc));
         page.getMarginalia().forEach(a -> index(col, book, image, a, doc));
         page.getUnderlines().forEach(a -> index(col, book, image, a, doc));
-        
-        result.add(doc);
     }
-
-    // Create document with generic annotation info
-    private Document create_document(AnnotatedPage page, BookCollection col, Book book, BookImage image) {
+    
+    // Create document to index a canvas
+    private Document create_canvas_document(BookCollection col, Book book, BookImage image) {
         Document doc = new Document();
 
         String collection_id = get_uri(col.getId(), null, col.getId(), PresentationRequestType.COLLECTION);
         String manifest_id = get_uri(col.getId(), book.getId(), null, PresentationRequestType.MANIFEST);
         String canvas_id = get_uri(col.getId(), book.getId(), image.getName(), PresentationRequestType.CANVAS);
-        
+
         addField(doc, JHSearchField.COLLECTION_ID, collection_id);
-        
+
         addField(doc, JHSearchField.OBJECT_ID, canvas_id);
         addField(doc, JHSearchField.OBJECT_TYPE, IIIFNames.SC_CANVAS);
         addField(doc, JHSearchField.OBJECT_LABEL, image.getName());
-        
-        addField(doc, JHSearchField.MANIFEST_ID, manifest_id);        
+
+        addField(doc, JHSearchField.MANIFEST_ID, manifest_id);
         addField(doc, JHSearchField.MANIFEST_LABEL, book.getBookMetadata("en").getCommonName());
-        
+
+        return doc;
+    }
+
+    // Create document to index a manifest
+    private Document create_manifest_document(BookCollection col, Book book) {
+        Document doc = new Document();
+
+        String collection_id = get_uri(col.getId(), null, col.getId(), PresentationRequestType.COLLECTION);
+        String manifest_id = get_uri(col.getId(), book.getId(), null, PresentationRequestType.MANIFEST);
+        String manifest_label = book.getBookMetadata("en").getCommonName();
+
+        addField(doc, JHSearchField.COLLECTION_ID, collection_id);
+
+        addField(doc, JHSearchField.OBJECT_ID, manifest_id);
+        addField(doc, JHSearchField.OBJECT_TYPE, IIIFNames.SC_MANIFEST);
+        addField(doc, JHSearchField.OBJECT_LABEL, manifest_label);
+
+        addField(doc, JHSearchField.MANIFEST_ID, manifest_id);
+        addField(doc, JHSearchField.MANIFEST_LABEL, manifest_label);
+
         return doc;
     }
 
@@ -169,7 +379,7 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
 
     private void index(BookCollection col, Book book, BookImage image, Errata errata, Document doc) {
         SearchFieldType type = get_lang(errata);
-        
+
         addField(doc, JHSearchField.ERRATA, type, errata.getAmendedText());
         addField(doc, JHSearchField.ERRATA, type, errata.getReferencedText());
     }
@@ -181,7 +391,7 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
 
     private void index(BookCollection col, Book book, BookImage image, Numeral numeral, Document doc) {
         SearchFieldType type = get_lang(numeral);
-        
+
         addField(doc, JHSearchField.NUMERAL, type, numeral.getReferencedText());
         addField(doc, JHSearchField.NUMERAL, type, numeral.getNumeral());
     }
@@ -192,19 +402,19 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
 
     private SearchFieldType get_lang(Annotation a) {
         SearchFieldType type = null;
-        
+
         if (a.getLanguage() != null) {
             type = getSearchFieldTypeForLang(a.getLanguage());
         }
-        
+
         if (type == null) {
             // TODO Check book metadata for lang
             return SearchFieldType.ENGLISH;
         }
-        
+
         return type;
     }
-    
+
     private void index(BookCollection col, Book book, BookImage image, Marginalia marg, Document doc) {
         addField(doc, JHSearchField.MARGINALIA, get_lang(marg), marg.getReferencedText());
 
@@ -225,7 +435,7 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
             for (Position pos : lang.getPositions()) {
                 transcription.append(to_string(pos.getTexts()));
 
-                //Index <person> tags + variants
+                // Index <person> tags + variants
                 StringBuilder people = new StringBuilder("People: ");
                 pos.getPeople().forEach(person -> {
                     people.append(person).append(" ");
@@ -284,9 +494,10 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
         }
         return sb.toString();
     }
-    
-    // TODO Duplicated from BasePresentationTransformer. Must be put into separate service.
-    
+
+    // TODO Duplicated from BasePresentationTransformer. Must be put into
+    // separate service.
+
     private String get_uri(String collection, String book, String name, PresentationRequestType type) {
         return formatter.format(get_request(collection, book, name, type));
     }
@@ -295,8 +506,7 @@ public class JHSearchLuceneMapper extends BaseLuceneMapper {
         return collection + (book == null || book.isEmpty() ? "" : "." + book);
     }
 
-    private PresentationRequest get_request(String collection, String book, String name,
-                                                    PresentationRequestType type) {
+    private PresentationRequest get_request(String collection, String book, String name, PresentationRequestType type) {
         return new PresentationRequest(get_id(collection, book), name, type);
     }
 }
---
+++ rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/jhsearch/TranscriptionXMLReader.java
@@ -0,0 +1,188 @@
+package rosa.iiif.presentation.core.jhsearch;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+// TODO This is duplicated from rosa-website-search
+
+/**
+ * A SAX content handler that will parse Roman de la Rose XML transcription
+ * fragments to pull important data. Primarily used when indexing these
+ * transcriptions. As this handler parses XML, it will record text content
+ * for various categories such as poetry, lecoy, illustrations, etc which
+ * can be retrieved when parsing is complete.
+ *
+ * The stored data is maintained between parsing different documents or
+ * cleared using the {@link #clear()} method if the data should not be saved.
+ */
+public class TranscriptionXMLReader extends DefaultHandler {
+
+    private StringBuffer poetry;
+    private StringBuffer line;
+    private StringBuffer lecoy;
+    private StringBuffer rubric;
+    private StringBuffer catchphrase;
+    private StringBuffer illus;
+    private StringBuffer note;
+
+    private StringBuffer current = null;
+
+    public TranscriptionXMLReader() {
+        poetry = new StringBuffer();
+        line = new StringBuffer();
+        lecoy = new StringBuffer();
+        rubric = new StringBuffer();
+        catchphrase = new StringBuffer();
+        illus = new StringBuffer();
+        note = new StringBuffer();
+    }
+
+    /**
+     * Clear any saved text data generated parsing a document.
+     */
+    public void clear() {
+        poetry.delete(0, poetry.length());
+        line.delete(0, line.length());
+        lecoy.delete(0, lecoy.length());
+        rubric.delete(0, rubric.length());
+        catchphrase.delete(0, catchphrase.length());
+        illus.delete(0, illus.length());
+        note.delete(0, note.length());
+    }
+
+    public String getPoetry() {
+        return poetry.toString();
+    }
+
+    public String getLine() {
+        return line.toString();
+    }
+
+    public String getLecoy() {
+        return lecoy.toString();
+    }
+
+    public String getRubric() {
+        return rubric.toString();
+    }
+
+    public boolean hasRubric() {
+        return rubric.length() > 0;
+    }
+
+    public String getCatchphrase() {
+        return catchphrase.toString();
+    }
+
+    public boolean hasCatchphrase() {
+        return catchphrase.length() > 0;
+    }
+
+    public String getIllustration() {
+        return illus.toString();
+    }
+
+    public boolean hasIllus() {
+        return illus.length() > 0;
+    }
+
+    public String getNote() {
+        return note.toString();
+    }
+
+    public boolean hasNote() {
+        return note.length() > 0;
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        clear();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+        String val;
+
+        switch (qName) {
+            case "note":
+                val = getValue(atts, "type");
+
+                if (val == null || val.equals("scribalPun")) {
+                    break;
+                }
+
+
+                if (val.equals("character")) {
+                    illus.append(' ');
+                    current = illus;
+                } else {
+                    current = note;
+                }
+
+                break;
+            case "figure":
+                illus.append(' ');
+                current = illus;
+                break;
+            case "l":
+                line.append(' ');
+                line.append(getValue(atts, "n"));
+                poetry.append(' ');
+                current = poetry;
+                break;
+            case "milestone":
+                lecoy.append(' ');
+                lecoy.append(getValue(atts, "n"));
+                break;
+            case "hi":
+                val = getValue(atts, "rend");
+
+                if (val != null && val.equals("rubric")) {
+                    rubric.append(' ');
+                    current = rubric;
+                }
+
+                break;
+            case "fw":
+                catchphrase.append(' ');
+                current = catchphrase;
+                break;
+            case "head":
+            case "lg":
+            case "expan":
+            case "add":
+            case "del":
+            case "rdg":
+            case "app":
+            case "cb":
+            case "pb":
+            case "div":
+            case "gap":
+            case "desc":
+                break;
+            default:
+                System.err.println("Not handled " + qName);
+                break;
+        }
+    }
+
+    @Override
+    public void characters(char[] text, int offset, int len)
+            throws SAXException {
+        if (current != null) {
+            current.append(text, offset, len);
+        }
+    }
+
+    private static String getValue(Attributes atts, String name) {
+        for (int i = 0; i < atts.getLength(); i++) {
+            if (atts.getQName(i).equals(name)) {
+                return atts.getValue(i);
+            }
+        }
+
+        return null;
+    }
+
+}
