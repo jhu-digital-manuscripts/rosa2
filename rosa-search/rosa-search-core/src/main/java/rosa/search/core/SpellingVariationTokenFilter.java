153b925870114f6f6ae0fd2fd258abdc05e0f6e5
John Abrahams <jabrah23@gmail.com>
2016-05-05 13:39:16 -0400

    New toke filter and analyzer to handle Old French searching.

--- rosa-search/rosa-search-core/src/main/java/rosa/search/core/SpellingVariationTokenFilter.java
+++ rosa-search/rosa-search-core/src/main/java/rosa/search/core/SpellingVariationTokenFilter.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 /**
@@ -34,18 +35,41 @@ public class SpellingVariationTokenFilter extends TokenFilter {
         spellingEquivalence.put(key, new HashSet<>(Arrays.asList(variants)));
     }
 
-
     @Override
     public boolean incrementToken() throws IOException {
         if (input.incrementToken()) {
-            String buffer = new String(termAtt.buffer());
-            int len = termAtt.length();
+            for (Entry<String, Set<String>> entry : spellingEquivalence.entrySet()) {
+                String key = entry.getKey();
 
-
+                for (String variant : entry.getValue()) {
+                    doReplace(key, variant);
+                }
+            }
 
             return true;
         } else {
             return false;
+        }
+    }
+
+    private void doReplace(String replacement, String variant) {
+        char[] in_buff = termAtt.buffer();
+        char[] var_buff = variant.toCharArray();
+
+        int in_len = termAtt.length();
+        int var_len = variant.length();
+
+        for (int i = 0; i < in_len - var_len; i++) {
+            char[] in_frag = Arrays.copyOfRange(in_buff, i, var_len);
+
+            // If this fragment matches the variant, replace it with 'replacement'
+            if (Arrays.equals(in_frag, var_buff)) {
+                String prefix = new String(Arrays.copyOfRange(in_buff, 0, i));
+                String suffix = new String(Arrays.copyOfRange(in_buff, i + var_len, in_len));
+
+                termAtt.setEmpty();
+                termAtt.append(prefix).append(replacement).append(suffix);
+            }
         }
     }
 }
