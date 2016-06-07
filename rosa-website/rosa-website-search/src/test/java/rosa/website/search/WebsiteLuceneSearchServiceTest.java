184a005dccc0d122886ded123e949397645d0d5a
John Abrahams <jabrah23@gmail.com>
2016-06-07 15:44:38 -0400

    Remove test logging

--- rosa-website/rosa-website-search/src/test/java/rosa/website/search/WebsiteLuceneSearchServiceTest.java
+++ rosa-website/rosa-website-search/src/test/java/rosa/website/search/WebsiteLuceneSearchServiceTest.java
@@ -41,8 +41,6 @@ public class WebsiteLuceneSearchServiceTest extends BaseSearchTest {
         mapper = new WebsiteLuceneMapper();
         Path indexPath = tmpfolder.newFolder().toPath();
         service = new LuceneSearchService(indexPath, mapper);
-        System.out.println("[WebsiteLuceneSearchServiceTest#beforeClass] " +
-                "Setting up search index.\n\t\ttemp folder: " + indexPath.toString());
         service.update(store, VALID_COLLECTION);
     }
 
@@ -179,7 +177,6 @@ public class WebsiteLuceneSearchServiceTest extends BaseSearchTest {
     @Test
     public void testSearchDescription() throws Exception {
         // Search only matches English
-        System.out.println("[WebsiteLuceneSearchServiceTest#testSearchDescription] ");
         {
             SearchResult result = service.search(new Query(
                     WebsiteSearchFields.DESCRIPTION_TEXT, "morocco"), null);
@@ -202,11 +199,8 @@ public class WebsiteLuceneSearchServiceTest extends BaseSearchTest {
 
         // Search only matches French French
         {
-            Query q = new Query(WebsiteSearchFields.DESCRIPTION_TEXT, "supprimée");
-            System.out.println("Testing query:: \t\t" + q.toString());
-            SearchResult result = service.search(q, null);
-
-            System.out.println("Search results:: \t\t" + result.toString());
+            SearchResult result = service.search(
+                    new Query(WebsiteSearchFields.DESCRIPTION_TEXT, "supprimée"), null);
 
             assertNotNull(result);
             assertEquals(1, result.getTotal());
@@ -219,8 +213,6 @@ public class WebsiteLuceneSearchServiceTest extends BaseSearchTest {
             assertEquals(2, match.getContext().size());
             String field = match.getContext().get(0);
             String context = match.getContext().get(1);
-
-            System.out.println(context);
 
             assertEquals(WebsiteSearchFields.DESCRIPTION_TEXT.getFieldName(), field);
             assertTrue(context.contains("supprimée"));
