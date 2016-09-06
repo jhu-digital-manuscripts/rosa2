56b3a2d115d1a262ad60bc06f7c2435603142666
Mark Patton <mark.patton@gmail.com>
2016-09-06 13:23:49 -0400

    Add default serialVersionUID to FSIDataServlet

--- rosa-website/rosa-website-core/src/main/java/rosa/website/core/server/FSIDataServlet.java
+++ rosa-website/rosa-website-core/src/main/java/rosa/website/core/server/FSIDataServlet.java
@@ -23,6 +23,8 @@ import java.util.logging.Logger;
  */
 @Singleton
 public class FSIDataServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     private static final Logger logger = Logger.getLogger(FSIDataServlet.class.toString());
 
     private static final String TYPE_PAGES = "pages.fsi";
