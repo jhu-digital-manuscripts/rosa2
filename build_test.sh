f31488e1dff121befec9ec0ad4e47bfa2c10b687
Mark Patton <mark.patton@gmail.com>
2017-08-29 10:19:42 -0400

    Add missing ssl port to test build script.

--- build_test.sh
+++ build_test.sh
@@ -1,1 +1,1 @@
-mvn clean install -Diiif.pres.scheme=https -Diiif.pres.host=rosetest.library.jhu.edu -Diiif.image.scheme=https -Diiif.image.host=image.library.jhu.edu -Diiif.image.prefix=/iiif
+mvn clean install -Diiif.pres.scheme=https -Diiif.pres.port=443 -Diiif.pres.host=rosetest.library.jhu.edu -Diiif.image.scheme=https -Diiif.image.port=443 -Diiif.image.host=image.library.jhu.edu -Diiif.image.prefix=/iiif
