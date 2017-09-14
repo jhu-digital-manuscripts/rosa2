6008d02d5691033ff8594ac6cf66b99ff80ff83b
Mark Patton <mark.patton@gmail.com>
2017-09-14 15:01:15 -0400

    Update build script for production to use https.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mvn clean install -Diiif.pres.host=jdm.library.jhu.edu -Diiif.image.host=image.library.jhu.edu -Diiif.image.prefix=/iiif -Dfsi.url=http://image.library.jhu.edu/fsi/
+mvn clean install -Diiif.pres.scheme=https -Diiif.pres.port=443 -Diiif.pres.host=jdm.library.jhu.edu -Diiif.pres.prefix=/iiif-pres -Diiif.image.scheme=https -Diiif.image.port=443 -Diiif.image.host=image.library.jhu.edu -Diiif.image.prefix=/iiif
