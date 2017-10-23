c5f21d30fa9dd8c842e7f2e1de49d13db86b86a2
Mark Patton <mark.patton@gmail.com>
2017-10-23 13:14:18 -0400

    Fix IIIFPresentationServletConfigTest to rewrite properties file as before.

--- rosa-iiif/rosa-iiif-presentation-endpoint/src/test/java/rosa/iiif/presentation/endpoint/IIIFPresentationServletConfigTest.java
+++ rosa-iiif/rosa-iiif-presentation-endpoint/src/test/java/rosa/iiif/presentation/endpoint/IIIFPresentationServletConfigTest.java
@@ -1,7 +1,15 @@
 package rosa.iiif.presentation.endpoint;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
+import java.io.BufferedWriter;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.inject.Injector;
@@ -11,6 +19,37 @@ import rosa.iiif.presentation.core.transform.impl.AnnotationListTransformer;
 import rosa.iiif.presentation.core.transform.impl.PresentationTransformerImpl;
 
 public class IIIFPresentationServletConfigTest {
+    private static String props = "archive.path = /mnt\n"
+            + "iiif.pres.scheme = http\n"
+            + "iiif.pres.host = rosetest.library.jhu.edu\n" 
+            + "iiif.pres.port = 80\n"
+            + "iiif.pres.prefix = /iiifpres\n"
+            + "iiif.pres.max_cache_age = 10\n"
+            + "iiif.image.scheme = http\n"
+            + "iiif.image.host = rosetest.library.jhu.edu\n" 
+            + "iiif.image.port = 80\n"
+            + "iiif.image.prefix = /iiifimage\n";
+
+    /**
+     * Must write new 'iiifservlet.properties' with valid values because it has not been
+     * filtered by maven.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setup() throws Exception {
+        URL propsPath = getClass().getClassLoader().getResource("iiif-servlet.properties");
+
+        if (propsPath == null) {
+            fail("Failed to load 'iiif-servlet.properties'");
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(propsPath.toURI()))) {
+            writer.write(props);
+            writer.flush();
+        }
+    }
+
     /**
      * Ensure that expected objects can be injected.
      * 
@@ -24,7 +63,7 @@ public class IIIFPresentationServletConfigTest {
 
         AnnotationListTransformer listTrans = injector.getInstance(AnnotationListTransformer.class);
         assertNotNull("Failed to inject annotation list transformer.", listTrans);
-        
+
         IIIFPresentationService service = injector.getInstance(IIIFPresentationService.class);
         assertNotNull("Failed to inject IIIF Service.", service);
 
