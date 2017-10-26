da5144a88ffe75b9e05bed677a2cba8c55e74ebf
John Abrahams <jabrah23@gmail.com>
2017-10-26 14:13:21 -0400

    IT test now checks that search info fields are not empty

--- rosa-iiif/rosa-iiif-presentation-endpoint/src/test/java/rosa/iiif/presentation/endpoint/ITIIIFPresentationServlet.java
+++ rosa-iiif/rosa-iiif-presentation-endpoint/src/test/java/rosa/iiif/presentation/endpoint/ITIIIFPresentationServlet.java
@@ -1,12 +1,15 @@
 package rosa.iiif.presentation.endpoint;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -54,6 +57,34 @@ public class ITIIIFPresentationServlet {
         }
     }
 
+    private void check_retrieve_search_inro(String url) throws Exception {
+        HttpURLConnection con = (HttpURLConnection) (new URL(url)).openConnection();
+
+        con.connect();
+        int code = con.getResponseCode();
+        assertEquals(200, code);
+
+        try (InputStream is = con.getInputStream()) {
+            check_search_info_syntax(is);
+        }
+    }
+
+    private void check_search_info_syntax(InputStream is) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
+        JsonNode base = objectMapper.readTree(is);
+
+        check_for_json_array("fields", base);
+        check_for_json_array("categories", base);
+    }
+
+    // Check that a JSON field is an array and is not empty (has at least one element)
+    private void check_for_json_array(String fieldName, JsonNode baseObj) {
+        assertTrue(baseObj.hasNonNull(fieldName));
+        assertTrue(baseObj.findValue(fieldName).isArray());
+        assertNotNull(baseObj.findValue(fieldName).get(0));
+    }
+
     /**
      * Test that each book and collection can be retrieved successfully through
      * the IIIF Presentation API.
@@ -73,16 +104,18 @@ public class ITIIIFPresentationServlet {
     
     /**
      * Ensure that search info can be retrieved for each book and collection.
+     * Make sure that the 'fields' and 'categories' properties of the search
+     * info are not empty.
      * 
      * @throws Exception
      */
     @Test
     public void testSearchInfo() throws Exception {
         for (String col : store.listBookCollections()) {
-            check_retrieve_json(pres_uris.getCollectionURI(col) + JHSearchService.INFO_RESOURCE_PATH);
+            check_retrieve_search_inro(pres_uris.getCollectionURI(col) + JHSearchService.INFO_RESOURCE_PATH);
 
             for (String book : store.listBooks(col)) {
-                check_retrieve_json(pres_uris.getManifestURI(col, book) + JHSearchService.INFO_RESOURCE_PATH);
+                check_retrieve_search_inro(pres_uris.getManifestURI(col, book) + JHSearchService.INFO_RESOURCE_PATH);
             }
         }
     }
