9b11d65cba9587fe7fa5d4124fe0c89144b7adf6
Mark Patton <mark.patton@gmail.com>
2018-09-11 14:25:10 -0400

    Remove unused import.

--- rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/transform/impl/CollectionTransformer.java
+++ rosa-iiif/rosa-iiif-presentation-core/src/main/java/rosa/iiif/presentation/core/transform/impl/CollectionTransformer.java
@@ -2,7 +2,6 @@ package rosa.iiif.presentation.core.transform.impl;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
